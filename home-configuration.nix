13dfdc3accd5f0d1ce0d351a3d4a655f7a102922
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-12-09 01:46:08 -0300

    Automatically activate extensions

--- home-configuration.nix
+++ home-configuration.nix
@@ -9,11 +9,15 @@ in
       (import "${home-manager}/nixos")
     ];
 
-  users.users.jl.isNormalUser = true;
+  # (Optional: You can remove this line if it's already in configuration.nix)
+  users.users.jl.isNormalUser = true; 
+
   home-manager.users.jl = { pkgs, ... }: {
+    
     home.packages = [ 
 
       #Gnome extensions
+
       gnomeExtensions.blur-my-shell
       gnomeExtensions.caffeine
       gnomeExtensions.clipboard-indicator
@@ -26,10 +30,34 @@ in
       gnomeExtensions.hide-the-dock-in-overview
       gnomeExtensions.search-light
 
-    ];
-    programs.bash.enable = true;
+    ]; 
 
+    #Automatically activate extensions
+    dconf.settings = {
+      "org/gnome/shell" = {
+        disable-user-extensions = false;
+
+        enabled-extensions = with pkgs.gnomeExtensions; [
+          blur-my-shell.extensionUuid
+          caffeine.extensionUuid
+          clipboard-indicator.extensionUuid
+          gsconnect.extensionUuid
+          coverflow-alt-tab.extensionUuid
+          reboottouefi.extensionUuid
+          dynamic-panel.extensionUuid
+          gamebar-overlay.extensionUuid
+          hide-top-bar.extensionUuid
+          hide-the-dock-in-overview.extensionUuid
+          search-light.extensionUuid
+        ];
+      };
+
+      #Config Extensions
+      "org/gnome/shell/extensions/blur-my-shell" = {
+      };
+    };
+
+    programs.bash.enable = true;
     home.stateVersion = "25.11";
   };
 }
-
