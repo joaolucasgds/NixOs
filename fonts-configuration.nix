6c22e56c8505d75d1ebfe97a3b1f495707a428f0
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-12-10 08:00:47 -0300

    Add fonts config

---
+++ fonts-configuration.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+
+{
+  fonts.fontDir.enable = true;
+
+  fonts.packages = with pkgs; [
+    #Alacritty
+    nerd-fonts.fira-code
+
+    #Japanese / Chinese / Korean Support (CJK)
+    #noto-fonts-cjk-sans
+    #noto-fonts-cjk-serif
+
+    noto-fonts-color-emoji
+    roboto
+    ubuntu_font_family
+  ];
+}
