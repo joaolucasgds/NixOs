64c741854f3c9e32d601ad65b2b14f333dc318d9
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-12-12 15:24:53 -0300

    Uncomment desired fonts

--- fonts-configuration.nix
+++ fonts-configuration.nix
@@ -8,8 +8,8 @@
     nerd-fonts.fira-code
 
     #Japanese / Chinese / Korean Support (CJK)
-    #noto-fonts-cjk-sans
-    #noto-fonts-cjk-serif
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
 
     noto-fonts-color-emoji
     roboto
