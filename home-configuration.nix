2a09c0242932253a353d533c735fcec8f72504de
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-12-14 00:35:42 -0300

    Add fastfetch

--- home-configuration.nix
+++ home-configuration.nix
@@ -16,7 +16,7 @@ in
     nixpkgs.config.allowUnfree = true;
 
     home.packages = with pkgs; [ 
-      #Programing
+      #Programming
       neovim
       ripgrep 
       fd    
@@ -228,6 +228,7 @@ in
 
     };
     
+    #Programming
     programs.bash = {
       enable = true;
       initExtra = ''echo "Welcome to NixOs"'';
@@ -326,6 +327,72 @@ in
       enable = true;
       userName = "Joao Lucas Gomes de Souza"; 
       userEmail = "joaolucasgdesouza@gmail.com";
+    };
+
+    #Tool
+    programs.fastfetch = {
+      enable = true;
+      settings = {
+	logo = {
+	  padding = {
+	    top = 2;
+	    left = 1;
+	    right = 2;
+	  };
+	  color = {
+	    "1" = "white";
+	    "2" = "white";
+	    "3" = "white";
+	    "4" = "white";
+	    "5" = "white";
+	    "6" = "white";
+	    "7" = "white";
+	    "8" = "white";
+	    "9" = "white";
+	  };
+	};
+	display = {
+	  color = "black";
+	};
+	modules = [
+	  {
+	    type = "title";
+	    color = {
+	      user = "black";
+	      at = "black";
+	      host = "white";
+	    };
+	  }
+	  "separator"
+	  "os"
+	  "host"
+	  "kernel"
+	  "uptime"
+	  "packages"
+	  "shell"
+	  "display"
+	  "de"
+	  "wm"
+	  "wmtheme"
+	  "theme"
+	  "icons"
+	  "font"
+	  "cursor"
+	  "terminal"
+	  "terminalfont"
+	  "cpu"
+	  "gpu"
+	  "memory"
+	  "swap"
+	  "disk"
+	  "localip"
+	  "battery"
+	  "poweradapter"
+	  "locale"
+	  "break"
+	  "colors"
+	];
+      };
     };
 
     home.stateVersion = "25.11";
