94fdf8d1e9705945e0cb1bf49e1ec9dfedb5df56
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-12-19 23:05:49 -0300

    set up imports inside default.nix

---
+++ home-config/default.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }:
+
+{
+    imports = [
+	#Core
+	./homepkgs.nix
+	./dconf.nix
+	./xdg.nix
+
+	#Programs
+	./programs/alacritty.nix
+	./programs/bash.nix
+	./programs/fastfetch.nix
+	./programs/git.nix
+	./programs/lazygit.nix
+    ];
+}
+
