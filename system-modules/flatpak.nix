7ef77a0ad7d4803c7ac3de1e17e31adb167bcc91
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-12-31 03:06:45 -0300

    Correct bug

--- system-modules/flatpak.nix
+++ system-modules/flatpak.nix
@@ -1,36 +1,34 @@
-{ config, pkgs, ... }
-
-let
-  nix-flatpak = builtins.fetchTarball {
-    url = "https://github.com/gmodena/nix-flatpak/archive/main.tar.gz";
-  };
-in
+{ config, pkgs, ... }:
 
 {
-    services.flatpak = {
-	enable = true;
+  imports = [
+    "${builtins.fetchTarball "https://github.com/gmodena/nix-flatpak/archive/v0.5.2.tar.gz"}/modules/nixos.nix"
+  ];
 
-	uninstallUnmanaged = true; 
+  services.flatpak = {
+    enable = true;
+    
+    uninstallUnmanaged = true; 
 
-	update.auto = {
-	    enable = true;
-	    onCalendar = "weekly"; 
-	};
-
-	packages = [
-	    # --- Browsing & Media ---
-	    "app.zen_browser.zen"
-	    "io.freetubeapp.FreeTube"
-	    "com.github.iwalton3.jellyfin-media-player"
-
-	    # --- Tools & Utilities ---
-	    "com.dec05eba.gpu_screen_recorder"          
-	    "app.drey.Warp"                             
-	    "net.nokyan.Resources"                     
-
-	    # --- Gaming ---
-	    "com.heroicgameslauncher.hgl"               
-	    "org.vinegarhq.Sober"                      
-	];
+    update.auto = {
+      enable = true;
+      onCalendar = "weekly"; 
     };
+
+    packages = [
+      # --- Browsing & Media ---
+      "app.zen_browser.zen"
+      "io.freetubeapp.FreeTube"
+      "com.github.iwalton3.jellyfin-media-player"
+
+      # --- Tools & Utilities ---
+      "com.dec05eba.gpu_screen_recorder"
+      "app.drey.Warp"
+      "net.nokyan.Resources"
+
+      # --- Gaming ---
+      "com.heroicgameslauncher.hgl"
+      "org.vinegarhq.Sober"
+    ];
+  };
 }
