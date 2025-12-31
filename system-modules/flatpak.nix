8b7a52906281a4679712bc85e27ee18adbbe3502
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2025-12-31 03:24:05 -0300

    add 4 lines of indentation

--- system-modules/flatpak.nix
+++ system-modules/flatpak.nix
@@ -1,34 +1,34 @@
 { config, pkgs, ... }:
 
 {
-  imports = [
-    "${builtins.fetchTarball "https://github.com/gmodena/nix-flatpak/archive/v0.5.2.tar.gz"}/modules/nixos.nix"
-  ];
-
-  services.flatpak = {
-    enable = true;
-    
-    uninstallUnmanaged = true; 
-
-    update.auto = {
-      enable = true;
-      onCalendar = "weekly"; 
-    };
-
-    packages = [
-      # --- Browsing & Media ---
-      "app.zen_browser.zen"
-      "io.freetubeapp.FreeTube"
-      "com.github.iwalton3.jellyfin-media-player"
-
-      # --- Tools & Utilities ---
-      "com.dec05eba.gpu_screen_recorder"
-      "app.drey.Warp"
-      "net.nokyan.Resources"
-
-      # --- Gaming ---
-      "com.heroicgameslauncher.hgl"
-      "org.vinegarhq.Sober"
+    imports = [
+        "${builtins.fetchTarball "https://github.com/gmodena/nix-flatpak/archive/v0.5.2.tar.gz"}/modules/nixos.nix"
     ];
-  };
+
+    services.flatpak = {
+        enable = true;
+
+        uninstallUnmanaged = true;
+
+        update.auto = {
+            enable = true;
+            onCalendar = "weekly";
+        };
+
+        packages = [
+            # --- Browsing & Media ---
+            "app.zen_browser.zen"
+            "io.freetubeapp.FreeTube"
+            "com.github.iwalton3.jellyfin-media-player"
+
+            # --- Tools & Utilities ---
+            "com.dec05eba.gpu_screen_recorder"
+            "app.drey.Warp"
+            "net.nokyan.Resources"
+
+            # --- Gaming ---
+            "com.heroicgameslauncher.hgl"
+            "org.vinegarhq.Sober"
+        ];
+    };
 }
