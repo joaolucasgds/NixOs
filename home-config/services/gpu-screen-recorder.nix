ace71c3bd71117b0c21529746dc8b661f87e3170
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-01-06 23:00:41 -0300

    Correct service syntax

--- home-config/services/gpu-screen-recorder.nix
+++ home-config/services/gpu-screen-recorder.nix
@@ -1,22 +1,22 @@
 { config, pkgs, ... }:
 
 {
-    systemd.user.services."gpu-screen-recorder-ui" = {
-	text = ''
-	    [Unit]
-	    Description=GPU Screen Recorder UI Service
-	    After=graphical-session.target
-
-	    [Service]
-	    Environment=PATH=/run/current-system/sw/bin:/home/jl/.nix-profile/bin:/usr/bin:/bin
-	    ExecStart=/run/current-system/sw/bin/flatpak run com.dec05eba.gpu_screen_recorder gsr-ui
-	    KillSignal=SIGINT
-	    Restart=on-failure
-	    RestartSec=5s
-
-	    [Install]
-	    WantedBy=graphical-session.target
-	'';
+  systemd.user.services."gpu-screen-recorder-ui" = {
+    Unit = {
+      Description = "GPU Screen Recorder UI Service";
+      After = [ "graphical-session.target" ];
     };
-}
 
+    Service = {
+      Environment = "PATH=/run/current-system/sw/bin:/home/jl/.nix-profile/bin:/usr/bin:/bin";
+      ExecStart = "/run/current-system/sw/bin/flatpak run com.dec05eba.gpu_screen_recorder gsr-ui";
+      KillSignal = "SIGINT";
+      Restart = "on-failure";
+      RestartSec = "5s";
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+}
