034701624976f2665cc1da737fb729da53c772c3
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-01-15 02:34:46 -0300

    Correct indentation

--- home-config/default.nix
+++ home-config/default.nix
@@ -2,23 +2,24 @@
 
 {
     imports = [
-	#Core
-	./homepkgs.nix
-	./dconf.nix
-	./xdg.nix
-	./wallpapers.nix
+        #Core
+        ./homepkgs.nix
+        ./dconf.nix
+        ./xdg.nix
+        ./wallpapers.nix
 
-	#Systemd services
-	./services/gpu-screen-recorder.nix
+        #Systemd services
+        ./services/gpu-screen-recorder.nix
 
-	#Programs
-	./programs/alacritty.nix
-	./programs/bash.nix
-	./programs/fastfetch.nix
-	./programs/git.nix
-	./programs/lazygit.nix
-	./programs/vesktop.nix
-	./programs/bruh.nix
+        #Programs
+        ./programs/alacritty.nix
+        ./programs/bash.nix
+        ./programs/fastfetch.nix
+        ./programs/git.nix
+        ./programs/lazygit.nix
+        ./programs/nvim.nix
+        ./programs/vesktop.nix
+        ./programs/bruh.nix
     ];
 }
 
