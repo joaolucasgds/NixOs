d2638d00c311181e27cf16c0a82155adc62db480
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-01-16 23:16:34 -0300

    Correct indentation in bruh.nix homepkg

--- modules/home-config/programs/bruh.nix
+++ modules/home-config/programs/bruh.nix
@@ -2,25 +2,25 @@
 
 {
     home.packages = [
-	(pkgs.writeShellScriptBin "bruh" ''
-	    operator=$1
-	    value=$2
-	    laptopDisplayMinimunBrightnessValue=50
+	    (pkgs.writeShellScriptBin "bruh" ''
+            operator=$1
+            value=$2
+            laptopDisplayMinimunBrightnessValue=50
 
-	    # KILL previous instances so they don't fight for the I2C bus
-	    pkill -f "ddcutil --display 1 setvcp 10" || true
+            # KILL previous instances so they don't fight for the I2C bus
+            pkill -f "ddcutil --display 1 setvcp 10" || true
 
-	    ${pkgs.brightnessctl}/bin/brightnessctl set $value$operator
+            ${pkgs.brightnessctl}/bin/brightnessctl set $value$operator
 
-	    ${pkgs.ddcutil}/bin/ddcutil --display 1 setvcp 10 "$operator" "$value" > /dev/null 2>&1 &
+            ${pkgs.ddcutil}/bin/ddcutil --display 1 setvcp 10 "$operator" "$value" > /dev/null 2>&1 &
 
-	    #The choice to make the lowest value possible on the laptop screen be "50" was made because 50 is equal to my main monitor 0 value;
-	    #Guess i will have to change this if i ever get another laptop
-	    currentLaptopDisplayBrightnessValue=$(${pkgs.brightnessctl}/bin/brightnessctl get)
+            #The choice to make the lowest value possible on the laptop screen be "50" was made because 50 is equal to my main monitor 0 value;
+            #Guess i will have to change this if i ever get another laptop
+            currentLaptopDisplayBrightnessValue=$(${pkgs.brightnessctl}/bin/brightnessctl get)
 
-	    if [ "$currentLaptopDisplayBrightnessValue" -lt "$laptopDisplayMinimunBrightnessValue" ]; then
-		${pkgs.brightnessctl}/bin/brightnessctl set $laptopDisplayMinimunBrightnessValue
-	    fi
+            if [ "$currentLaptopDisplayBrightnessValue" -lt "$laptopDisplayMinimunBrightnessValue" ]; then
+            ${pkgs.brightnessctl}/bin/brightnessctl set $laptopDisplayMinimunBrightnessValue
+            fi
         '')
     ];
 }
