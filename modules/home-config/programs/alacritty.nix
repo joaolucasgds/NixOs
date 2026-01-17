ed3ee78f68f7906de047eb3a3326c0142a1538c3
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-01-16 23:09:51 -0300

    Comment out padding config

--- modules/home-config/programs/alacritty.nix
+++ modules/home-config/programs/alacritty.nix
@@ -1,4 +1,4 @@
-{ pkgs, ... }:
+{ ... }:
 
 {
     programs.alacritty = {
@@ -8,10 +8,10 @@
                 opacity = 0.1;
                 blur = true;
                 decorations = "None";
-                padding = {
-                    x = 8;
-                    y = 8;
-                };
+                #padding = {
+                #x = 8;
+                #y = 8;
+                #};
             };
 
             font = {
