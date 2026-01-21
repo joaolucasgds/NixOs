c0ae9f98f2944e10b4a8d82c080909c1a6767c96
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-01-21 02:31:28 -0300

    Correct indentation on gpu-screen-recorder.nix

--- modules/home-config/services/gpu-screen-recorder.nix
+++ modules/home-config/services/gpu-screen-recorder.nix
@@ -1,22 +1,22 @@
 { ... }:
 
 {
-  systemd.user.services."gpu-screen-recorder-ui" = {
-    Unit = {
-      Description = "GPU Screen Recorder UI Service";
-      After = [ "graphical-session.target" ];
-    };
+    systemd.user.services."gpu-screen-recorder-ui" = {
+        Unit = {
+            Description = "GPU Screen Recorder UI Service";
+            After = [ "graphical-session.target" ];
+        };
 
-    Service = {
-      Environment = "PATH=/run/current-system/sw/bin:/home/jl/.nix-profile/bin:/usr/bin:/bin";
-      ExecStart = "/run/current-system/sw/bin/flatpak run com.dec05eba.gpu_screen_recorder gsr-ui";
-      KillSignal = "SIGINT";
-      Restart = "on-failure";
-      RestartSec = "5s";
-    };
+        Service = {
+            Environment = "PATH=/run/current-system/sw/bin:/home/jl/.nix-profile/bin:/usr/bin:/bin";
+            ExecStart = "/run/current-system/sw/bin/flatpak run com.dec05eba.gpu_screen_recorder gsr-ui";
+            KillSignal = "SIGINT";
+            Restart = "on-failure";
+            RestartSec = "5s";
+        };
 
-    Install = {
-      WantedBy = [ "graphical-session.target" ];
+        Install = {
+            WantedBy = [ "graphical-session.target" ];
+        };
     };
-  };
 }
