3a44a6d0f518d9493798ef0a446143e4a505fd46
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-01-21 01:59:34 -0300

    Correct indentation on fonts.nix

--- modules/system/fonts.nix
+++ modules/system/fonts.nix
@@ -1,18 +1,18 @@
 { pkgs, ... }:
 
 {
-  fonts.fontDir.enable = true;
+    fonts.fontDir.enable = true;
 
-  fonts.packages = with pkgs; [
-    #Alacritty
-    nerd-fonts.fira-code
+    fonts.packages = with pkgs; [
+        #Alacritty
+        nerd-fonts.fira-code
 
-    #Japanese / Chinese / Korean Support (CJK)
-    noto-fonts-cjk-sans
-    noto-fonts-cjk-serif
+        #Japanese / Chinese / Korean Support (CJK)
+        noto-fonts-cjk-sans
+        noto-fonts-cjk-serif
 
-    noto-fonts-color-emoji
-    roboto
-    ubuntu-classic
-  ];
+        noto-fonts-color-emoji
+        roboto
+        ubuntu-classic
+    ];
 }
