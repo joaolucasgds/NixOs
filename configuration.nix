bf367ce1799eab1a489f79e88deff6101198f751
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-01-21 01:52:42 -0300

    Correct indentation on configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -18,104 +18,104 @@
         # Maybe use default isntead?
     ];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
 
-  boot.kernelPackages = pkgs.linuxPackages_zen;
+    boot.kernelPackages = pkgs.linuxPackages_zen;
 
-  networking.hostName = "NixOs"; 
+    networking.hostName = "NixOs"; 
 
-  # Enable networking
-  networking.networkmanager.enable = true;
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+    # Enable networking
+    networking.networkmanager.enable = true;
+    # Configure network proxy if necessary
+    # networking.proxy.default = "http://user:password@proxy:port/";
+    # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
-  time.timeZone = "America/Sao_Paulo";
+    time.timeZone = "America/Sao_Paulo";
 
-  i18n.defaultLocale = "en_US.UTF-8";
+    i18n.defaultLocale = "en_US.UTF-8";
 
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "pt_BR.UTF-8";
-    LC_IDENTIFICATION = "pt_BR.UTF-8";
-    LC_MEASUREMENT = "pt_BR.UTF-8";
-    LC_MONETARY = "pt_BR.UTF-8";
-    LC_NAME = "pt_BR.UTF-8";
-    LC_NUMERIC = "pt_BR.UTF-8";
-    LC_PAPER = "pt_BR.UTF-8";
-    LC_TELEPHONE = "pt_BR.UTF-8";
-    LC_TIME = "pt_BR.UTF-8";
-  };
+    i18n.extraLocaleSettings = {
+        LC_ADDRESS = "pt_BR.UTF-8";
+        LC_IDENTIFICATION = "pt_BR.UTF-8";
+        LC_MEASUREMENT = "pt_BR.UTF-8";
+        LC_MONETARY = "pt_BR.UTF-8";
+        LC_NAME = "pt_BR.UTF-8";
+        LC_NUMERIC = "pt_BR.UTF-8";
+        LC_PAPER = "pt_BR.UTF-8";
+        LC_TELEPHONE = "pt_BR.UTF-8";
+        LC_TIME = "pt_BR.UTF-8";
+    };
 
-  # Enable the X11 windowing system.
-  services.xserver.enable = true;
-  services.xserver.excludePackages = [ pkgs.xterm ];
+    # Enable the X11 windowing system.
+    services.xserver.enable = true;
+    services.xserver.excludePackages = [ pkgs.xterm ];
 
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "br";
-    variant = "";
-  };
+    # Configure keymap in X11
+    services.xserver.xkb = {
+        layout = "br";
+        variant = "";
+    };
 
-  # Configure console keymap
-  console.keyMap = "br-abnt2";
+    # Configure console keymap
+    console.keyMap = "br-abnt2";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = false;
+    # Enable CUPS to print documents.
+    services.printing.enable = false;
 
-  #Needed for ddcutil
-  hardware.i2c.enable = true;
+    #Needed for ddcutil
+    hardware.i2c.enable = true;
 
-  # Enable sound with pipewire.
-  services.pulseaudio.enable = false;
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    # Enable sound with pipewire.
+    services.pulseaudio.enable = false;
+    security.rtkit.enable = true;
+    services.pipewire = {
+        enable = true;
+        alsa.enable = true;
+        alsa.support32Bit = true;
+        pulse.enable = true;
+        # If you want to use JACK applications, uncomment this
+        #jack.enable = true;
+    
+        # use the example session manager (no others are packaged yet so this is enabled by default,
+        # no need to redefine it in your config for now)
+        #media-session.enable = true;
+    };
 
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
-  };
+    users.users.jl = {
+        description = "João Lucas";
+        extraGroups = [ "networkmanager" "wheel" "i2c"];
+        initialPassword = "123";
+    };
 
-  users.users.jl = {
-    description = "João Lucas";
-    extraGroups = [ "networkmanager" "wheel" "i2c"];
-    initialPassword = "123";
-  };
+    users.users.root = {
+        initialPassword = "123"; #Set up actual passwords on install
+    };
 
-  users.users.root = {
-    initialPassword = "123"; #Set up actual passwords on install
-  };
+    security.sudo.extraConfig = ''
+        Defaults rootpw
+    '';
 
-  security.sudo.extraConfig = ''
-    Defaults rootpw
-  '';
+    # Some programs need SUID wrappers, can be configured further or are
+    # started in user sessions.
+    # programs.mtr.enable = true;
+    # programs.gnupg.agent = {
+    #   enable = true;
+    #   enableSSHSupport = true;
+    # };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+    # List services that you want to enable:
 
-  # List services that you want to enable:
+    # Enable the OpenSSH daemon.
+    # services.openssh.enable = true;
 
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "26.05"; # Did you read the comment?
-  documentation.nixos.enable = false;
+    # This value determines the NixOS release from which the default
+    # settings for stateful data, like file locations and database versions
+    # on your system were taken. It‘s perfectly fine and recommended to leave
+    # this value at the release version of the first install of this system.
+    # Before changing this value read the documentation for this option
+    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+    system.stateVersion = "26.05"; # Did you read the comment?
+    documentation.nixos.enable = false;
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
