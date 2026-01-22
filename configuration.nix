c87d86dd6c651c90816782f6b66f31aa98ee7b67
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-01-21 22:55:56 -0300

    Delete comments on configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -28,25 +28,6 @@
         Defaults rootpw
     '';
 
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
     system.stateVersion = "26.05"; # Did you read the comment?
     documentation.nixos.enable = false;
     nix.settings.experimental-features = [ "nix-command" "flakes" ];
