dfbabb8aeafcaa4ebecdf3176410fcc7e4706d33
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-02-06 15:27:48 -0300

    Update cleanhome.nix service

--- modules/home-config/services/cleanhome.nix
+++ modules/home-config/services/cleanhome.nix
@@ -1,9 +1,42 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
-    systemd.user.tmpfiles.rules = [
-        "d      %h/Downloads         -    -    -     7d   -"
-        #Maybe add more directories to clean? this could prove to be a really useful tool for system maintenance
-    ];
+    systemd.user.services.downloads-cleaner = {
+        Unit = {
+            Description = "Delete Downloads older than 7 days (Ignoring Access Time)";
+        };
+
+        Service = {
+            Type = "oneshot";
+            # EXPLANATION:
+            # -maxdepth 1: Only look at the files/folders sitting directly in Downloads
+            # -mindepth 1: Don't delete the "Downloads" folder itself
+            # -mtime +7:  Check ONLY if 'Modified' > 7 days ago (Ignores Access Time)
+            # -exec rm -rf: Forcefully cut the neck of anything found
+            ExecStart = "${pkgs.findutils}/bin/find %h/Downloads -xdev -P -maxdepth 1 -mindepth 1 -mtime +7 -exec rm -rf -- {} +";
+
+            NoNewPrivileges = true;
+            PrivateTmp = true;
+            ProtectSystem = "strict";
+            ProtectHome = "read-only";
+            ReadWritePaths = [ "%h/Downloads" ];
+        };
+    };
+
+    systemd.user.timers.downloads-cleaner = {
+        Unit = {
+            Description = "Run the cleaner daily";
+        };
+
+        Timer = {
+            OnCalendar = "daily";
+            Persistent = true;      # Run immediately if computer was off during the scheduled time
+            RandomizedDelaySec = "5m"; # Avoid spike at startup
+        };
+
+        Install = {
+            WantedBy = [ "timers.target" ];
+        };
+    };
 }
 
