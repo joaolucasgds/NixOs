6684388f0e2896e0d40202cf63ae7cc77f88fc38
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-02-24 18:14:11 -0300

    feat: configure commit verification

--- modules/home-config/programs/git.nix
+++ modules/home-config/programs/git.nix
@@ -1,25 +1,35 @@
 { pkgs, ... }:
 
+let
+    email = "joaolucasgdesouza@gmail.com";
+    pubKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBn0yHKuVATx44H+tjDsw1eFwyDi+EqcWbuecVNCk0LG GitHub Signing Key";
+in
+
 {
     programs.git = {
-	    enable = true;
+        enable = true;
+
         settings = {
             user = {
                 name = "Joao Lucas Gomes de Souza";
-                email = "joaolucasgdesouza@gmail.com";
+                email = email;
             };
+
+            gpg = {
+                format = "ssh";
+                ssh.allowedSignersFile = "~/.config/git/allowed_signers";
+            };
+
+            "gpg \"ssh\"".program = "${pkgs.openssh}/bin/ssh-keygen";
         };
 
         signing = {
             key = "~/.ssh/github-signing.pub";
             signByDefault = true;
         };
-
-        extraConfig = {
-            gpg.format = "ssh";
-            "gpg \"ssh\"".program = "${pkgs.openssh}/bin/ssh-keygen"; # Explicitly use the SSH binary
-        };
-
     };
-}
 
+    home.file.".config/git/allowed_signers".text = ''
+        ${email} ${pubKey}
+    '';
+}
