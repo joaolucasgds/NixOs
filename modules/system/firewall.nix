91230544c7dbd6f11a58267060b02f69d12ec4d7
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-01 21:00:24 -0300

    Open Minecraft server port

--- modules/system/firewall.nix
+++ modules/system/firewall.nix
@@ -4,6 +4,10 @@
     networking.firewall = rec {
         enable = true;
 
+        # Minecraft
+        allowedTCPPorts = [ 25565 ];
+        allowedUDPPorts = allowedTCPPorts;
+
         #KDE Connect
         allowedTCPPortRanges = [ { from = 1714; to = 1764; } ];
         allowedUDPPortRanges = allowedTCPPortRanges;
