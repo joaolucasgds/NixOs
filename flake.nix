9b7b0450ba55279d783c86bc374ed97611e453d8
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-01 23:08:56 -0300

    Add loq as one of the possible outputs on Flake.nix

--- flake.nix
+++ flake.nix
@@ -38,11 +38,11 @@
 
     outputs = { self, nixpkgs, ... }@inputs: {
 
-        nixosConfigurations.NixOs = nixpkgs.lib.nixosSystem{
+        "loq" = nixpkgs.lib.nixosSystem{
             system = "x86_64-linux";
             specialArgs = { inherit inputs; };
             modules = [
-                ./configuration.nix
+                ./hosts/loq/configuration.nix
             ];
         };
 
