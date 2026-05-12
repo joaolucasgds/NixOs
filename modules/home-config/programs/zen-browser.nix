0e9a8be2d801bb08a4582aa4405fe282cac9f1b6
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-11 22:32:06 -0300

    Declare some zen-browser extensions

--- modules/home-config/programs/zen-browser.nix
+++ modules/home-config/programs/zen-browser.nix
@@ -7,6 +7,9 @@
         enable = true;
 
         policies = {
+
+            DefaultSearchEngine = "DuckDuckGo";
+
             AutofillAddressEnabled = true;
             AutofillCreditCardEnabled = false;
             DisableAppUpdate = true;
@@ -22,6 +25,28 @@
                 Locked = true;
                 Cryptomining = true;
                 Fingerprinting = true;
+            };
+
+            ExtensionSettings = {
+                "uBlock0@raymondhill.net" = {
+                    install_url = "https://addons.mozilla.org/firefox/downloads/latest/ublock-origin/latest.xpi";
+                    installation_mode = "force_installed";
+                };
+
+                "{6b733b82-9261-47ee-a595-2dda294a4d08}" = {
+                    install_url = "https://addons.mozilla.org/firefox/downloads/latest/yomitan/latest.xpi";
+                    installation_mode = "force_installed";
+                };
+
+                "addon@darkreader.org" = {
+                    install_url = "https://addons.mozilla.org/firefox/downloads/latest/darkreader/latest.xpi";
+                    installation_mode = "force_installed";
+                };
+                
+                "{d7742d87-e61d-4b78-b8a1-b469842139fa}" = {
+                    install_url = "https://addons.mozilla.org/firefox/downloads/latest/vimium-ff/latest.xpi";
+                    installation_mode = "force_installed";
+                };
             };
         };
     };
