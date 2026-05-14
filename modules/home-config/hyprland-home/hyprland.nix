57cd0e3fb263fb18f1d4a6fc142c9eb897edb9e7
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-14 17:25:11 -0300

    Fix keyboard special buttons commands

--- modules/home-config/hyprland-home/hyprland.nix
+++ modules/home-config/hyprland-home/hyprland.nix
@@ -69,6 +69,19 @@ in
                 "$mainMod, mouse_down, workspace, e-1"
             ];
 
+            bindel = [
+                ", XF86AudioRaiseVolume, exec, wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+"
+                ", XF86AudioLowerVolume, exec, wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-"
+            ];
+
+            bindl = [
+                ", XF86AudioMute, exec, wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle"
+                ", XF86AudioMicMute, exec, wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle"
+                ", XF86AudioPlay, exec, playerctl play-pause"
+                ", XF86AudioNext, exec, playerctl next"
+                ", XF86AudioPrev, exec, playerctl previous"
+            ];
+
             input = {
                 kb_layout = "br";
                 accel_profile = "flat";
