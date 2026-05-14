fcfd7a0bb56db7f8c3bbe7c42409fa1ba2c0f13a
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-14 18:07:04 -0300

    Configure bash to integrate well with yazi

--- modules/home-config/programs/bash.nix
+++ modules/home-config/programs/bash.nix
@@ -3,7 +3,18 @@
 {
     programs.bash = {
 	    enable = true;
-	    initExtra = ''fastfetch'';
+	    initExtra = ''
+            fastfetch
+
+            function y() {
+                local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
+                yazi "$@" --cwd-file="$tmp"
+                if cwd="$(command cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then
+                    builtin cd -- "$cwd"
+                fi
+                rm -f -- "$tmp"
+            }
+        '';
         bashrcExtra = ''
             export PS1="\[\033[38;5;4m\]\u@\h \w \$ \[\033[0m\]"
         '';
