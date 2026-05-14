4d84d0d636e3c8da606e8356f4d510a65a3e72bb
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-14 18:45:57 -0300

    Make mpv stop closing automatically

--- modules/home-config/programs/mpv.nix
+++ modules/home-config/programs/mpv.nix
@@ -3,6 +3,10 @@
 {
     programs.mpv = {
         enable = true;
+        config = {
+            image-display-duration = "inf"; # Keeps images open forever
+            keep-open = "yes";              # Pauses videos on the last frame instead of closing
+        };
         #Make sure images are zoomable
         extraInput = ''
             Ctrl+WHEEL_UP add video-zoom 0.1
