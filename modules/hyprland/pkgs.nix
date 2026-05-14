451936a940ffb1ae4386c352381736fd89b11256
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-14 18:04:22 -0300

    Modify mpv pkg so images are zoomable

--- modules/hyprland/pkgs.nix
+++ modules/hyprland/pkgs.nix
@@ -9,5 +9,10 @@
     programs.kdeconnect.enable = true;
     programs.mpv = {
         enable = true;
+        #Make sure images are zoomable
+        extraInput = ''
+            Ctrl+WHEEL_UP add video-zoom 0.1
+            Ctrl+WHEEL_DOWN add video-zoom -0.1
+        '';
     };
 }
