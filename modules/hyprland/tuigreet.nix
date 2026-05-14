f27c72cc3132bdc81aead5960aefa3eb76f95ad1
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-14 01:33:31 -0300

    tuigreet starts hyprland

--- modules/hyprland/tuigreet.nix
+++ modules/hyprland/tuigreet.nix
@@ -5,7 +5,7 @@
         enable = true;
         settings = {
             default_session = {
-                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd 'uwsm start hyprland-uwsm.desktop'";
+                command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --remember --cmd 'hyprland'";
                 user = "greeter";
             };
         };
