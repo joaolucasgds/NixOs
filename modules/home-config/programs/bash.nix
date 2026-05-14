dfbfc2a965a538f56aa26de9ce4165fbeb507acb
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-14 00:10:50 -0300

    aling bash with mutagen

--- modules/home-config/programs/bash.nix
+++ modules/home-config/programs/bash.nix
@@ -4,5 +4,8 @@
     programs.bash = {
 	    enable = true;
 	    initExtra = ''fastfetch'';
+        bashrcExtra = ''
+            export PS1="\[\033[38;5;4m\]\u@\h \w \$ \[\033[0m\]"
+        '';
     };
 }
