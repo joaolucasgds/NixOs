7432e0f895506e88574d85edf3fa45e0c9bbc42e
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-05-14 21:16:45 -0300

    tmux: Make windows/panes start with current pwd and hide status bar by default

--- modules/home-config/programs/tmux.nix
+++ modules/home-config/programs/tmux.nix
@@ -6,8 +6,15 @@
         mouse = true;
 
         extraConfig = ''
+            # --- 1. Open new windows and panes in the current directory ---
+            bind c new-window -c "#{pane_current_path}"
+            bind '"' split-window -v -c "#{pane_current_path}"
+            bind % split-window -h -c "#{pane_current_path}"
+
             set-option -g status-style bg=white,fg=black
 
+            # Hide the status bar by default
+            set -g status off
             # Toggle status bar visibility with Prefix + b
             bind-key b set-option -g status
         '';
