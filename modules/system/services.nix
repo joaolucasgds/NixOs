c461e94de7441a2bfa4eecf804b977d2022af71b
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-06-02 16:09:27 -0300

    Add openssh to services.nix system module

--- modules/system/services.nix
+++ modules/system/services.nix
@@ -1,27 +1,42 @@
-{ ... }:
+__:
 
 {
-    # Firmware updates
-    services.fwupd.enable = true;
+    services = {
+        # Firmware updates
+        fwupd.enable = true;
 
-    # Ssd trimming
-    services.fstrim.enable = true;
+        # Ssd trimming
+        fstrim.enable = true;
 
-    # Low Memory Killer
-    services.earlyoom = {
-        enable = true;
-        enableNotifications = true;
-        freeMemThreshold = 5;
-        extraArgs = [
-            "-g" 
-            "--avoid"
-            "^(steam|gamescope|java|minecraft|nvim|neovim)$"
-        ];
+        # Low Memory Killer
+        earlyoom = {
+            enable = true;
+            enableNotifications = true;
+            freeMemThreshold = 5;
+            extraArgs = [
+                "-g" 
+                "--avoid"
+                "^(steam|gamescope|java|minecraft|nvim|neovim)$"
+            ];
+        };
+
+        openssh = {
+            enable = true;
+            openFirewall = true;
+            startWhenNeeded = true;
+            settings = {
+                PasswordAuthentication = false;
+                KbdInteractiveAuthentication = false;
+                PermitRootLogin = "no";
+                AllowUsers = [ "jl" ];
+                MaxAuthTries = 3;
+            };
+        };
+
+        # Printing (Disabled for now)
+        printing.enable = false;
+
+        #sched_ext maybe i will try this again someday
     };
-
-    # Printing (Disabled for now)
-    services.printing.enable = false;
-
-    #sched_ext maybe i will try this again someday
 }
 
