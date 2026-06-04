90f63bee3e71a776e30970848907e901ec8e9c4d
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-06-03 22:09:38 -0300

    Remove profile picture from dms lock screen

--- modules/home-config/services/dmsSettings.nix
+++ modules/home-config/services/dmsSettings.nix
@@ -26,6 +26,7 @@ let
         windSpeedUnit = "ms";
         use24HourClock = false;
         lockAtStartup = true;
+        lockScreenShowProfileImage = false;
 
         # --- Cursor ---
         cursorSettings = {
