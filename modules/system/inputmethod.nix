c760864759cbb8995f3c131710bd67102e267874
Joao Lucas Gomes de Souza <joaolucasgdesouza@gmail.com>
2026-09-18 21:23:25 -0300

    Add fcitx5-chinese-addons to fcitx5 addons in inputmethod.nix

--- modules/system/inputmethod.nix
+++ modules/system/inputmethod.nix
@@ -8,6 +8,7 @@
         fcitx5.addons = with pkgs; [
             fcitx5-mozc             # Japanese
             fcitx5-chewing # Zhuyin
+            qt6Packages.fcitx5-chinese-addons
             fcitx5-gtk              # Essential for GTK applications
         ];
     };
